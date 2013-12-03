4f7ffad29597dda47829ae4692d84c3252307bf8
Darren Hoo <darren.hoo@gmail.com>
2013-12-03 16:14:08 +0800

    forkpty creates many zombie telnet processes after they exit explicitly set SIG_CHLD handler to SIG_IGN.

--- main.m
+++ main.m
@@ -13,5 +13,8 @@
 
 int main(int argc, char *argv[])
 {
+    if (signal(SIGCHLD, SIG_IGN) == SIG_ERR)
+        perror("signal error");
+    
     return NSApplicationMain(argc,  (const char **)argv);
 }
